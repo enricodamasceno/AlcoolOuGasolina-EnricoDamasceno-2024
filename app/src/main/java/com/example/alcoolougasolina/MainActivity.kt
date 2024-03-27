830bd05d4265137594ed336b9b13a223b96061a0
Enrico Damasceno <118639665+enricodamasceno@users.noreply.github.com>
2024-03-27 14:11:32 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -6,24 +6,60 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    var gasolinaText:String = ""
+    var alcoolText:String = ""
+    var gasolina:Double = 0.0
+    var alcool:Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            percentual = savedInstanceState.getDouble("percentual")
+            gasolina = savedInstanceState.getDouble("gasolina")
+            alcool = savedInstanceState.getDouble("alcool")
+        }
         Log.i("PDM24.1","No onCreate, $percentual")
 
-        percentual=0.7
-        val btCalc: Button = findViewById(R.id.btCalcular)
+        val swPercent: Switch = findViewById(R.id.swPercentual)
+
+        swPercent.setOnCheckedChangeListener { _, isChecked ->
+            percentual = if (isChecked) {
+                0.75
+            } else {
+                0.7
+            }
+        }
+
+        val btCalc:Button = findViewById(R.id.btCalcular)
         val textMsg:TextView= findViewById(R.id.textMsg)
-        //btCalc.setBackgroundColor(Color.CYAN)
+
+        // Código do evento:
         btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
-            Log.d("PDM24","No btCalcular, $percentual")
+
+            // Inicializando as variáveis dos preços:
+            gasolinaText = findViewById<EditText>(R.id.edGasolina).text.toString()
+            alcoolText = findViewById<EditText>(R.id.edAlcool).text.toString()
+
+            gasolina = gasolinaText.toDoubleOrNull() ?: 0.0
+            alcool = alcoolText.toDoubleOrNull() ?: 0.0
+
+            if (alcool <= percentual * gasolina) {
+                textMsg.text = "O álcool terá um rendimento maior."
+            } else {
+                textMsg.text = "A gasolina terá um rendimento maior."
+            }
+
+            Log.d("PDM24","No btCalcular, $percentual, $gasolina, $alcool")
         })
     }
 override fun onResume(){
@@ -45,5 +81,11 @@ override fun onStop(){
 override fun onDestroy(){
     super.onDestroy()
     Log.wtf("PDM24","No Destroy")
+}
+override fun onSaveInstanceState(outState: Bundle) {
+    outState.putDouble("percentual", percentual)
+    outState.putDouble("gasolina", gasolina)
+    outState.putDouble("alcool", alcool)
+    super.onSaveInstanceState(outState)
 }
 }
